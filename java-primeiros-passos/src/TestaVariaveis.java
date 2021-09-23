7add8a000e4bc078c52dc8ceaf397dc1ca0836ee
Gustavo Cardoso <gusstavocardoso@gamil.com>
2021-09-22 21:46:02 -0300

    The middle

---
+++ java-primeiros-passos/src/TestaVariaveis.java
@@ -0,0 +1,10 @@
+public class TestaVariaveis {
+    public static void main(String[] args) {
+        System.out.println("ola variáveis em java");
+
+        int idade;
+        idade = 35;
+
+        System.out.println(idade);
+    }
+}
